4b9c66067c32d92b882f8f51a6e23ca750969b3a
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-09-06 18:43:40 +0200

    Create HomeWork1.java

---
+++ HomeWork1.java
@@ -0,0 +1,34 @@
+/**
+ *Java basic, HomeWork1
+ *
+ @author Muzaffar Eshonqulov
+ @version 6.9.2022
+ *
+*/
+class HomeWork1 {
+    public static void main(String[] args) {
+        // integer types: int, byte, short, long
+        int a = 10; int b = 5;  int P =(a+b)*2; int S =a*b;
+        System.out.printf("P = %d%n", P);
+        System.out.printf("S = %d%n", S);
+        int x = 5; int y = 3; int sum = x + y; int sub = x - y; int mult = x * y; int div = x / y;
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div);
+        // arrays is references type
+        int[] array =new int[5];
+        array[0] = 6;
+        System.out.println(array[0]);
+        float z = a * b + 5;
+        System.out.println(z);
+        double t = P + S;
+        System.out.println(t);
+        System.out.printf("b = %d%n a = %d%n", b, a);
+        // integer types: int, byte, short, long
+        int k = 40; int r = 15; int d = 2 * r; int K = k* d;
+        System.out.println(d);
+        System.out.println(K);
+
+    }
+    }
