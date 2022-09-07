7537409ed5cb2e7c828afd0b4deaebb1d99b72bf
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-09-07 17:52:07 +0200

    Update HomeWork1.java

--- HomeWork1.java
+++ HomeWork1.java
@@ -8,27 +8,32 @@
 class HomeWork1 {
     public static void main(String[] args) {
         // integer types: int, byte, short, long
-        int a = 10; int b = 5;  int P =(a+b)*2; int S =a*b;
-        System.out.printf("P = %d%n", P);
-        System.out.printf("S = %d%n", S);
-        int x = 5; int y = 3; int sum = x + y; int sub = x - y; int mult = x * y; int div = x / y;
+        int a = 10; int b = 5; int p =(a+b)*2; int s =a*b;
+        System.out.println(p);
+        System.out.println(s);
+        // integer types: int, byte, short, long
+        int x = 5; int y = 3; int sum = x + y; int sub = x - y; int mult = x * y;
         System.out.println(sum);
         System.out.println(sub);
         System.out.println(mult);
-        System.out.println(div);
+        // noninteger types: float, double
+        float o = 5 / 3f;
+        System.out.println(o);
         // arrays is references type
         int[] array =new int[5];
         array[0] = 6;
         System.out.println(array[0]);
-        float z = a * b + 5;
-        System.out.println(z);
-        double t = P + S;
-        System.out.println(t);
-        System.out.printf("b = %d%n a = %d%n", b, a);
+        array[1] = 5;
+        System.out.println(array[1]);
+        array[2] = 3;
+        System.out.println(array[2]);
+        array[3] = 100;
+        System.out.println(array[3]);
+        array[4] = 50;
+        System.out.println(array[4]);
         // integer types: int, byte, short, long
-        int k = 40; int r = 15; int d = 2 * r; int K = k* d;
+        int k = 40; int r = 15; int d = 2 * r; int m = k* d;
         System.out.println(d);
-        System.out.println(K);
-
+        System.out.println(m);
     }
     }
