1ec78900e3fd680bac6144b690f87c68d3617e2c
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-09-11 22:48:22 +0200

    Update HomeWork2.java

--- HomeWork2.java
+++ HomeWork2.java
@@ -10,10 +10,10 @@ class HomeWork2 {
     public static void main (String[] args) {
         //Level 1
         //task1
-        System.out.println(checkWeather(-15, 0));
-        System.out.println(checkWeather(0, 18));
-        System.out.println(checkWeather(19, 29));
-        System.out.println(checkWeather(29, 35));
+        System.out.println(checkWeather(-15));
+        System.out.println(checkWeather(18));
+        System.out.println(checkWeather(28));
+        System.out.println(checkWeather(29));
         
         //task2-variant1
         boolean isWeekend = true;
@@ -23,7 +23,7 @@ class HomeWork2 {
         } else {
             System.out.println("I cannot walk");
         }
-        
+
         //task2-variant2
             System.out.println(canWalk(true, false));
 
@@ -33,25 +33,63 @@ class HomeWork2 {
         System.out.println(minus (11, 8));
         int d = multiply(15, 2);
         System.out.println(d);
-        System.out.println(divide(6, 3));
+        System.out.println(divide(5, 3));
+        
+        
+        //Level 2
+        
+        //task1(d)
+         int day = 3;
+        String dayOfWeek;
+        switch(day){
+        case 1: dayOfWeek = "Monday";
+            break;
+        case 2: dayOfWeek = "Tuesday";
+            break;
+        case 3: dayOfWeek = "Wednesday";
+            break;
+        case 4: dayOfWeek = "Thursday";
+            break;
+        case 5: dayOfWeek = "Friday";
+            break;
+        case 6: dayOfWeek = "Saturday";
+            break;
+        case 7: dayOfWeek = "Sunday";
+            break;
+        default: dayOfWeek = "error";
+            break;
+        }
+        System.out.println(dayOfWeek);
+
+        //task2 (e)
+        System.out.println(canBuyFood(true, false));
+
+        //task 3 (f)
+        double  a = 1;
+        double  b = 6;
+        double  z = 6;
+        double  x1 = 0;
+        double  x2 = 0;
+        x1 = (- b + Math.sqrt(b * b - 4 * a * z))/(2 * a);
+            System.out.println(x1);
     }
-    
-         // METHODS
+
+        //METHODS
 
         //task1
-    static int checkWeather(int t, int t1) {
-        if (t == -15 && t1 == 0) {
+    static int checkWeather(int t) {
+        if (t == -15 ) {
             System.out.println("It's frost");
             return t;
-        } else if( t == 0 && t1 == 18) {
+        } else if( t == 18) {
             System.out.println("It's  cold");
-            return t1;
-        } else if ( t == 19 && t1== 29) {
+            return t;
+        } else if ( t == 28) {
             System.out.println("It's  warm");
-            return t1;
-        } else if (t == 29 && t1 == 35) {
+            return t;
+        } else if (t == 29) {
             System.out.println("It's  hot");
-            return t1;
+            return t;
         } else {
             System.out.println("Error weather");
             return t;
@@ -60,7 +98,7 @@ class HomeWork2 {
 
         //task2-variant2
     static boolean canWalk(boolean isWeekend, boolean isRain) {
-        if (isWeekend && isRain) {
+        if (isWeekend) {
             System.out.println("i can walk");
             return true;
         } else {
@@ -81,8 +119,19 @@ class HomeWork2 {
         int d = a * b;
         return d;
     }
-    static int divide(int a, int b) {
+    static float divide(int a, int b) {
         int c = a / b;
         return c;
+    }
+       //Level 2
+       // task2(e)
+    static boolean canBuyFood(boolean isLidlOpen,boolean isTescoOpen) {
+        if (isLidlOpen) {
+            System.out.println("i can buy food in Tesco");
+            return true;
+        } else {
+            System.out.println("i cannot buy food");
+            return false;
+        }
     }
 }
