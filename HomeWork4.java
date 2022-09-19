d6e702018ce685600c39fe64a016551ce096cea5
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-09-19 23:26:09 +0200

    Update HomeWork4.java

--- HomeWork4.java
+++ HomeWork4.java
@@ -8,30 +8,30 @@ import java.util.Scanner;
  @date 18.09.2022
  *
 */
-
-
-
 class HomeWork4 {
     public static void main (String[] args) {
         Random random =new Random();
         Scanner scanner = new Scanner(System.in);
-        int number = random.nextInt(10);
-        int guess =1;
-        
-        for (int i =0; i < 3; i++) {
-            System.out.println("Guess number [0...9]:");
-             guess = scanner.nextInt();
-            if (guess <number) {
-                System.out.println("Your number is less");
-            }else if (guess > number) {
-                System.out.println("Your number is bigger");
-            } else {
-                System.out.println("You win, you guess my number");
-                break;
+        int number = 0;
+        int guess =-1;
+        do {
+            number = random.nextInt(10);
+            for (int i =0; i < 3; i++) {
+                System.out.print("Guess number [0...9]:");
+                guess = scanner.nextInt();
+                if (guess <number) {
+                    System.out.println("Your number is less");
+                }else if (guess > number) {
+                    System.out.println("Your number is bigger");
+                } else {
+                    System.out.println("You win, you guess my number");
+                    break;
+                }
             }
-        }
-        if (number !=guess) {
-            System.out.println("You lose");
-        }
+            if (number !=guess) {
+                System.out.println("You lose");
+            }
+            System.out.print("Repeat game : Yes-1: No-0" );
+        } while(scanner.nextInt()==1);
     }
 }
