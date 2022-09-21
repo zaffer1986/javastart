4e590c4768980e7de2e092b097356a22abccedb2
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-09-21 12:26:05 +0200

    Create HomeWork4.java

---
+++ HomeWork4.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+import java.util.Scanner;
+/**
+ *Java basic: HomeWork4
+ *
+ @author Muzaffar Eshonqulov
+ @todo 14.09.2022
+ @date 20.09.2022
+ *
+*/
+class HomeWork4 {
+    public static void main (String[] args) {
+        Random random =new Random();
+        // Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        int guess =-1;
+        do {
+            number = random.nextInt(10);
+            for (int i =0; i < 3; i++) {
+                System.out.print("Guess number [0...9]:");
+                guess = scanner.nextInt();
+                if (guess <number) {
+                    System.out.println("Your number is less");
+                }else if (guess > number) {
+                    System.out.println("Your number is bigger");
+                } else {
+                    System.out.println("You win, you guess my number");
+                    break;
+                }
+            }
+            if (number !=guess) {
+                System.out.println("You lose");
+            }
+            System.out.print("Repeat game : Yes-1: No-0" );
+        } while(scanner.nextInt()==1);
+        
+        
+        //level2
+        int number = 0;
+        int guess =-1;
+        do {
+            number = random.nextInt(10);
+            for (int i =0; i < 3; i++) {
+                System.out.print("Guess number [0...9]:");
+                guess = scanner.nextInt();
+                if (guess <number) {
+                    System.out.println("Your number is less");
+                }else if (guess > number) {
+                    System.out.println("Your number is bigger");
+                } else {
+                    System.out.println("You win, you guess my number");
+                    break;
+                }
+            }
+            if (number !=guess) {
+                System.out.println("You lose");
+            }
+            System.out.print("Repeat game : Yes-1: No-0" );
+        } while(scanner.nextInt()==1);
+    }
+}
