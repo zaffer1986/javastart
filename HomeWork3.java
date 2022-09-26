7991a883ed91801158687abc0f092acb9b72617a
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-09-27 00:37:44 +0200

    Update HomeWork3.java

--- HomeWork3.java
+++ HomeWork3.java
@@ -1,16 +1,18 @@
+
 /**
  *Java basic: HomeWork3
  *
  @author Muzaffar Eshonqulov
- @todo 16.9.2022
- @date 12.9.2022
+ @todo 12.09.2022
+ @date 27.09.2022
+ *Please check line from 70 to 120
  *
 */
 class HomeWork3 {
     public static void main (String[] args) {
         //Level-1
         //task
-        int[] arr= {10,20,30,40,50};
+        /*int[] arr= {10,20,30,40,50};
         printArray(arr);
         
         //task2
@@ -26,6 +28,12 @@ class HomeWork3 {
         System.out.println("original from last number:");
         for(int j = invert.length -1; j > 0; j--) 
             System.out.print(invert[j] + " ");
+        
+        int[] invArray={0, 1, 1, 0, 0, 0, 1, 1};
+        printArray(invArray);
+        printArray(invertArray(invArray));
+        
+
     }
     //Methods Level 1
     //task1
@@ -50,4 +58,63 @@ class HomeWork3 {
         return max;
     }
     //task3
+    static int[] invertArray(int[] array) {
+        int[] result = new int[array.length];
+        for (int i = 1; i < array.length; i++) {
+            result[i]=1-array[i];
+        }
+        return result;
+        */
+        // Level 2
+        // task1
+        int[] array={5, 6, 8, 55, 66, 85, 3, -1, -5, 6};
+        System.out.print("Array length before sorting:\n");
+        for(int i=0; i< array.length; i++){
+        System.out.println(array[i]);
+        }
+        sortArray(array, array.length);
+        System.out.print("Array length after sorting:\n");
+        for(int i=0; i< array.length; i++){
+            System.out.println(array[i]);
+        }
+        //task2
+        int[] sortedArray = bubleSort(array, true);
+        System.out.println(sortedArray);
+        System.out.println(bubleSort(array,false));
+        
+        
+    }
+    //Level 2
+    //task1
+    static int[] sortArray(int[] array, int n) {
+        int[] result = new int[array.length];
+        for(int i=1; i < n; i++){
+            int j =i;
+            int a =array[i];
+        
+            while ((j > 0) && (array[j-1]>a)) {
+                array[j] = array[j-1];
+                j--;
+            }
+            array[j]=a;
+            result[j] = array[j];
+        }
+        return result;
+    }
+    //task2
+    static int[] bubleSort(int[] array, boolean asc) {
+        int[] result = new int[array.length];
+        System.arraycopy(array,0,result,0,array.length);
+        for (int i =result.length-1; i>0; i--) {
+            for (int j=0; j<i; j++){
+                if ((result[j]>result[j+1] && asc)
+                || (result[j]< result[j+1] && !asc)) {
+                int tmp = result[j];
+                result[j]=result[j+1];
+                result[j+1]=tmp;
+                }
+            }
+        }
+        return result;
+    }
 }
