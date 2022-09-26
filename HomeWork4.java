7e60bd464f687e986b608234d319355fc85bcbf8
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-09-27 01:22:35 +0200

    Update HomeWork4.java

--- HomeWork4.java
+++ HomeWork4.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
  *
  @author Muzaffar Eshonqulov
  @todo 14.09.2022
- @date 20.09.2022
+ @date 27.09.2022
+ *Please check line from 40 to 90
  *
 */
 class HomeWork4 {
@@ -37,7 +38,7 @@ class HomeWork4 {
     */
     
     //level2
-    
+    //task1
         int number = 0;
         int guess =-1;
         do {
@@ -55,6 +56,31 @@ class HomeWork4 {
                 }
             }
             if (number !=guess) {
+                System.out.println("You lose");
+            }
+            System.out.print("Repeat game : Yes-Y: No-N:" );
+        } while(scanner.next().equals("Y"));
+
+    
+    
+    //task 2
+        int colors = 0;
+        int find =-1;
+        do {
+            colors = random.nextInt(6);
+            for (int i =0; i < 3; i++) {
+                System.out.print("Guess color [white,blue,green,dark,yellow,red]:");
+                find = scanner.nextInt();
+                if (find <colors) {
+                    System.out.println("Your color is white");
+                }else if (find > colors) {
+                    System.out.println("Your color is green");
+                } else {
+                    System.out.println("You win, you guess color");
+                    break;
+                }
+            }
+            if (colors !=find){
                 System.out.println("You lose");
             }
             System.out.print("Repeat game : Yes-Y: No-N:" );
