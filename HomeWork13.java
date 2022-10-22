8e24483bc39fbd9538d3c7c3399d4ec39442828d
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-10-22 11:06:37 +0200

    Create HomeWork13.java

---
+++ HomeWork13.java
@@ -0,0 +1,26 @@
+class HomeWork13{
+    
+    1.Что понравилось в курсе?
+    Good explanation from tutor,Coding and get result fot it, creating games, using swing,.. 
+
+    2.Что не понравилось?
+     few homeworks, but for workers its enough.
+
+    3.Что "пошло легко", было понятно?
+    Coping last lessons, 
+
+    4.Что "пошло с трудом", было непонятно?
+    massive inside if,while,do, creating big massives, random, scanner, but everyone should practice a lot, me too.
+
+    5.Ваши предложения: как можно было бы улучшить курс?
+    to give more homeworks, and give the result of homework, not comment, but to show mistakes in coding
+    Thank you
+ 
+    
+    
+    
+    
+    
+    
+
+}
